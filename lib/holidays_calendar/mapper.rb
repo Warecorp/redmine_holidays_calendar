e7d07749d43eefdc2162e16b72d744911ee8737f
Alex Klimenkov <alex.klimenkov89@gmail.com>
2014-06-06 17:23:28 +0300

    update Mapper for HolidaysCalendar

--- lib/holidays_calendar/mapper.rb
+++ lib/holidays_calendar/mapper.rb
@@ -2,9 +2,7 @@ module HolidaysCalendar
   class Mapper
     def self.dates_day_off
       configuration = Setting.plugin_redmine_holidays_calendar
-      if configuration['dates_day_off']
-        return configuration['dates_day_off']
-      end
+      configuration && configuration['dates_day_off'] ? configuration['dates_day_off'] : ''
     end
   end
 end
