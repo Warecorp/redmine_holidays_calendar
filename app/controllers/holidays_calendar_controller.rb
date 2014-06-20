351a8d13ce769336bb5c8abbf81185d3727bf4fd
Alex Klimenkov <alex.klimenkov89@gmail.com>
2014-06-20 16:02:30 +0300

    add rest to calendar

---
+++ app/controllers/holidays_calendar_controller.rb
@@ -0,0 +1,11 @@
+class HolidaysCalendarController < ApplicationController
+  accept_api_auth :calendar
+  before_filter :require_admin
+
+  def calendar
+    respond_to do |format|
+      format.json { render json: {days_off: HolidaysCalendar::Mapper.dates_day_off} }
+    end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,3 @@
-# Plugin's routes
-# See: http://guides.rubyonrails.org/routing.html
+RedmineApp::Application.routes.draw do
+  get 'redmine_holidays_calendar/calendar' => 'holidays_calendar#calendar'
+end
